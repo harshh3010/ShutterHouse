40c5c7e9b4922ee4ab2a091bedc8a0be34f6c45b
Harsh Gyanchandani <harsh.gyanchandani@gmail.com>
2020-07-17 16:48:27 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:shutterhouse/screens/welcome_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id : (context) => WelcomeScreen(),
+      },
+    );
+  }
+}
+
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+
+  static final String id = 'welcome_screen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('Hello'),
+      ),
+    );
+  }
+}
