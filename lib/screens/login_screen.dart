84fe176094f71c346ea8856e2f45705f8910b558
Harsh Gyanchandani <harsh.gyanchandani@gmail.com>
2020-07-17 18:08:56 +0530

    added functionality in welcome_screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  static final String id = 'login_screen';
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
