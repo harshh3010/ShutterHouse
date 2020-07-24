56b8305be5bfa0f20e44fb20cd1675ec504ccb9a
Harsh Gyanchandani <harsh.gyanchandani@gmail.com>
2020-07-24 19:31:14 +0530

    designed product screen

--- lib/components/number_label.dart
+++ lib/components/number_label.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class NumberLabel extends StatelessWidget {
 
   final String label,value;
-  NumberLabel({@required this.label,@required this.value});
+  final Color labelColor,valueColor;
+  NumberLabel({@required this.label,@required this.value,@required this.labelColor,@required this.valueColor});
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +15,7 @@ class NumberLabel extends StatelessWidget {
           value,
           style: TextStyle(
             fontSize: 24,
-            color: Colors.white,
+            color: valueColor,
             fontFamily: 'Proxima Nova',
             fontWeight: FontWeight.bold,
           ),
@@ -26,7 +27,7 @@ class NumberLabel extends StatelessWidget {
           label,
           style: TextStyle(
             fontFamily: 'Proxima Nova',
-            color: Colors.blueGrey.shade400,
+            color: labelColor,
             fontSize: 12.0,
             fontWeight: FontWeight.w900,
           ),
--- lib/components/product_card.dart
+++ lib/components/product_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shutterhouse/model/product.dart';
+import 'package:shutterhouse/screens/product_screen.dart';
 import 'package:shutterhouse/utilities/constants.dart';
 
 class ProductCard extends StatelessWidget {
@@ -11,74 +12,86 @@ class ProductCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30,vertical: 10),
-      child: Container(
-        padding: EdgeInsets.all(20),
-        width: double.infinity,
-        height: 140,
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10.0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.08),
-                blurRadius: 3.0,
+      child: GestureDetector(
+        onTap: (){
+          Navigator.push(context,MaterialPageRoute(builder: (context) => ProductScreen(product: product,image: Image.network(product.imageURL),)));
+        },
+        child: Container(
+          padding: EdgeInsets.all(20),
+          width: double.infinity,
+          height: 140,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.08),
+                  blurRadius: 3.0,
+                ),
+              ]),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'product_image',
+                child: Image(
+                  image: NetworkImage(product.imageURL),
+                  width: 100,
+                ),
               ),
-            ]),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Image(
-              image: NetworkImage(product.imageURL),
-              width: 100,
-            ),
-            SizedBox(
-              width: 15,
-            ),
-            Expanded(
-              child: Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    Text(
-                      product.name,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: 'Proxima Nova',
-                        fontWeight: FontWeight.w900,
-                        color: Colors.grey.shade800,
-                      ),
-                    ),
-                    Row(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(
-                          'Rs. ${product.cost}',
+              SizedBox(
+                width: 15,
+              ),
+              Expanded(
+                child: Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Hero(
+                  tag: 'product_name',
+                        child: Text(
+                          product.name,
                           style: TextStyle(
                             fontSize: 20,
                             fontFamily: 'Proxima Nova',
                             fontWeight: FontWeight.w900,
-                            color: kColorRed,
+                            color: Colors.grey.shade800,
+                              decoration: TextDecoration.none,
                           ),
                         ),
-                        Text(
-                          '⭐ ${product.rating}',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontFamily: 'Proxima Nova',
-                            fontWeight: FontWeight.w900,
-                            color: Colors.grey.shade800,
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            'Rs. ${product.cost}',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontFamily: 'Proxima Nova',
+                              fontWeight: FontWeight.w900,
+                              color: kColorRed,
+                            ),
                           ),
-                        )
-                      ],
-                    )
-                  ],
+                          Text(
+                            '⭐ ${product.rating}',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: 'Proxima Nova',
+                              fontWeight: FontWeight.w900,
+                              color: Colors.grey.shade800,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
